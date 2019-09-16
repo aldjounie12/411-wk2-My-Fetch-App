f99f0a76ef5db2a1ac9b97d52eebefb22d705c3a
Gina Gabriel <aldjounie12@yahoo.com>
2019-09-16 18:10:57 -0500

    My Fetch App

--- src/App.js
+++ src/App.js
@@ -1,41 +1,16 @@
 import React from 'react';
-// import logo from './logo.svg';
+// import Beers from './logo.svg';
 import './App.css';
-import Main from './components/Main'
-
-class App extends component {
-  state = {
-    beers: []
-  }
-
-  componentDidMount () {
-console.log('test')
-  }
-  render() {
-    return (
-    <div className='App'>
-      <Main/>
-    </div>
-    );
-  }
-}
-
-
-export default App;
-
-import React from 'react';
-import Beers from './Beers';
-import './App.css';
+import Beers from './components/Beers'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
       <Beers/>
-
-      </header>
-    </div>
+     </div>
   );
 }
 
 export default App;
+
+
